b51ac3d6a5c719bc9350dd4b365930e81ee1a397
LeylaY1996 <leylay1996@gmail.com>
2020-11-02 00:26:38 +0300

    project init

---
+++ src/components/Characters.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+    <div>
+        <h3>Hello this is characters.vue</h3>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Characters'
+}
+</script>
+<style lang="css" scoped>
+
+</style>
